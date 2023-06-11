ccce41175705865182947a73fb74dd80e6cd285c
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2023-06-11 19:19:41 -0300

    Add files via upload

---
+++ Valores.js
@@ -0,0 +1,24 @@
+var CampoValor = 1;
+function AdicionarValor(){
+    var search = `valor${CampoValor}`;
+    var InputAtual = document.getElementById(search).value;
+    console.log(search, InputAtual)
+    if(InputAtual > 0){
+        CampoValor++;
+        document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups">
+        <label for="Inserir Valor${CampoValor}:"><input type="text" placeholder="Informe o valor${CampoValor}" name="Valor${CampoValor}" id="valor${CampoValor}">
+        </div>`)
+    }else{
+        if(CampoValor == 1){
+            document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups"><label for="Inserir Valor${CampoValor}:"><button type="submit">Enviar</button></label></div>`)
+            document.getElementById("BotaoAdiciona").disabled = true;
+        }else{
+            document.getElementById(`${search}`).remove()
+            document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups"><label for="Inserir Valor${CampoValor}:"><button type="submit">Enviar</button></label></div>`)
+            document.getElementById("BotaoAdiciona").disabled = true;
+        }
+        
+    
+         
+    
+}}
---
+++ Valores.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valores</title>
+    <script src="Valores.js"></script>
+</head>
+<body>
+    <form action="Valores1.php" method="post">
+        <div id="Formulario">
+            <div class="FormGroups">
+                <label for="Inserir Valor:1">
+                    <input type="text" placeholder="Informe o valor1" name="Valor1" id="valor1" >
+                    <button type="button" onclick="AdicionarValor()" id="BotaoAdiciona">Adicionar outro valor</button>
+                </label>
+            </div>
+        </div>
+    </form>  
+</body>
+</html>
