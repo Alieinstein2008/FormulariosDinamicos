89f0eabb91a1da0584190fd6b66d9939095aaa85
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2025-03-28 21:54:48 -0300

    Update Valores.js
    Indexação e melhorias no código e certas funcionalidades

--- Valores.js
+++ Valores.js
@@ -1,24 +1,35 @@
 var CampoValor = 1;
-function AdicionarValor(){
+function AdicionarValor() {
     var search = `valor${CampoValor}`;
     var InputAtual = document.getElementById(search).value;
-    console.log(search, InputAtual)
-    if(InputAtual > 0){
+    if (InputAtual > 0) {
         CampoValor++;
         document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups">
-        <label for="Inserir Valor${CampoValor}:"><input type="text" placeholder="Informe o valor${CampoValor}" name="Valor${CampoValor}" id="valor${CampoValor}">
+        <label for="Inserir Valor${CampoValor}:"><input type="number" autocomplete="off" placeholder="Informe o valor${CampoValor}" name="Valor${CampoValor}" id="valor${CampoValor}" step="0.01">
         </div>`)
-    }else{
-        if(CampoValor == 1){
-            document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups"><label for="Inserir Valor${CampoValor}:"><button type="submit">Enviar</button></label></div>`)
+    } else {
+        if (CampoValor == 1) {
+            document.getElementById('ButtonsBoard').insertAdjacentHTML('beforeend', `<input type="button" value="Enviar" id="BotaoEnviar"onclick="VerificarCampos()">`)
             document.getElementById("BotaoAdicionar").disabled = true;
-        }else{
+        } else {
             document.getElementById(`${search}`).remove()
-            document.getElementById('Formulario').insertAdjacentHTML('beforeend', `<div class="FormGroups"><label for="Inserir Valor${CampoValor}:"><button type="submit" id="BotaoEnviar">Enviar</button></label></div>`)
+            document.getElementById('ButtonsBoard').insertAdjacentHTML('beforeend', `<input type="button" value="Enviar" id="BotaoEnviar" onclick="VerificarCampos()">`)
             document.getElementById("BotaoAdicionar").disabled = true;
         }
-        
-    
-         
-    
-}}
+
+
+
+
+    }
+}
+function VerificarCampos() {
+    const CampoPrimarioId = "valor1";
+    const IdFormulario = "IdForm"
+    if (document.getElementById(CampoPrimarioId).value == '') {
+        alert("É necessário informar o valor de pelo menos um único campo, por favor preencha-o")
+    } else {
+        document.getElementById(IdFormulario).submit();
+    }
+
+
+}
