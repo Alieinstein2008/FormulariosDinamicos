25d9d9e22495f16bed897a37894e9b91fed4779d
Arthur Alonso <72287206+Alieinstein2008@users.noreply.github.com>
2025-03-28 21:55:37 -0300

    Update Valores1.php
    Indexação

--- Valores1.php
+++ Valores1.php
@@ -1,37 +1,37 @@
 <?php
-        $Cabecalho = "<table class='Tabela' border='1'><tr><td>Valor lido<td/><td>Quadrado<td/><td>Cubo<td/></tr>";
-        $CampoValor = 1;
-        $Valores = [];
-        do{
-            $url = "Valor".$CampoValor;
-            $Valores[$CampoValor] = $_POST[$url];
-            $CampoValor++;
-        }while(isset($_POST["Valor".$CampoValor]));
-        
-        echo "$Cabecalho" ;
-        $ContadorLinhas = 1;
-        foreach ($Valores as $Valor) {
-                if($ContadorLinhas % 21 == 0){
-                    echo "$Cabecalho";
-                    $Quadrado = $Valor ** 2;
-                    $Cubo = $Valor ** 3;
-                    echo "
+$Cabecalho = "<table class='Tabela' border='1'><tr><td>Valor lido<td/><td>Quadrado<td/><td>Cubo<td/></tr>";
+$CampoValor = 1;
+$Valores = [];
+do {
+    $url = "Valor" . $CampoValor;
+    $Valores[$CampoValor] = $_POST[$url];
+    $CampoValor++;
+} while (isset($_POST["Valor" . $CampoValor]));
+
+echo "$Cabecalho";
+$ContadorLinhas = 1;
+foreach ($Valores as $Valor) {
+    if ($ContadorLinhas % 21 == 0) {
+        echo "$Cabecalho";
+        $Quadrado = $Valor ** 2;
+        $Cubo = $Valor ** 3;
+        echo "
                     <tr>
                     <td>$Valor<td/>
                     <td>$Quadrado<td/>
                     <td>$Cubo<td/>
                     <tr/>";
-                }    
-                $Quadrado = $Valor ** 2;
-                $Cubo = $Valor ** 3;
-                echo "
+    }
+    $Quadrado = $Valor ** 2;
+    $Cubo = $Valor ** 3;
+    echo "
                 <tr>
                 <td>$Valor<td/>
                 <td>$Quadrado<td/>
                 <td>$Cubo<td/>
                 <tr/>";
-                $ContadorLinhas++;
-        }
-        echo "<table/>"
-        
-?>
+    $ContadorLinhas++;
+}
+echo "<table/>"
+
+    ?>
